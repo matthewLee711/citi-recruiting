751c266789bf4b83c91bc646024e45f706bd26b2
Nicholas Bailey <Bailey@Nicholass-MacBook.local>
2018-12-06 16:09:34 -0600

    updates routes to have correct screen names

--- citi-recruiting/navigation/MainTabNavigator.js
+++ citi-recruiting/navigation/MainTabNavigator.js
@@ -3,9 +3,9 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import HomeScreen from '../screens/AboutScreen';
+import LinksScreen from '../screens/LineScreen';
+import SettingsScreen from '../screens/UploadScreen';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
