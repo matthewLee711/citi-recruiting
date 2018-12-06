bbe9fd882c4dd8f0737ca3ca427e5723b29d069b
Nicholas Bailey <Bailey@Nicholass-MacBook.local>
2018-12-06 16:38:09 -0600

    added code for a nice button

--- citi-recruiting/screens/LineScreen.js
+++ citi-recruiting/screens/LineScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { Button } from 'react-native-elements';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -9,10 +10,21 @@ export default class LinksScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+      <ScrollView>
+        <ScrollView style={styles.container}>
+          {/* Go ahead and delete ExpoLinksView and replace it with your
+            * content, we just wanted to provide you with some helpful links */}
+          <ExpoLinksView />
+        </ScrollView>
+
+        <Button
+          icon={{
+            name: 'arrow-upward',
+            size: 15,
+            color: 'blue'
+          }}
+          title='I AM A BUTTON'
+        />
       </ScrollView>
     );
   }
